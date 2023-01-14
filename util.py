59de43aa4c7f944607a2ea4f9e27bffec3611927
Mikkel Aas <mikkel.aas@schibsted.com>
2023-01-15 00:36:48 +0100

    improve variable names

--- util.py
+++ util.py
@@ -2,15 +2,15 @@
 import dateutil.parser
 from datetime import datetime, timedelta
 
+
 # A function that takes the war end time and subtracts by the current time to get the time remaining of the war.
-def get_time(isoTime):
-
-    # The last four characters of the string are unecessary.
-    newISOTime = isoTime[:-4]
+def get_time(iso_time) -> str:
+    # The last four characters of the string are necessary.
+    new_iso_time = iso_time[:-4]
     now = dateutil.parser.isoparse(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    warTime = dateutil.parser.isoparse(newISOTime[:-5])
+    war_time = dateutil.parser.isoparse(new_iso_time[:-5])
 
-    # Adding two hours to the war time because it's in a different time zone.
-    warTime = warTime + timedelta(hours=2)
+    # Adding two hours to the wartime because it's in a different time zone.
+    war_time = war_time + timedelta(hours=2)
 
-    return str(warTime-now)
+    return str(war_time - now)
