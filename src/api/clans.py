35e44f919c7699eb72698945ae0951286b6435d2
Mikkel Aas <mikkel.aas@schibsted.com>
2023-01-15 22:09:26 +0100

    create fetch clan members function

--- src/api/clans.py
+++ src/api/clans.py
@@ -1,6 +1,7 @@
 import requests
 from models.Clan import Clan
 from models.War import War
+from models.Member import Member
 from requests import Response
 from api.constants import base_url, version, clan_tag, headers
 
@@ -33,3 +34,18 @@ def fetch_current_clan_war() -> War:
     war = War(data)
 
     return war
+
+
+def fetch_clan_members() -> list[Member]:
+    url = base_url + version + "/clans/%s" % clan_tag + "/members"
+
+    res: Response = requests.get(url, headers=headers)
+
+    if res.status_code != 200:
+        raise ValueError
+
+    data = res.json()
+
+    members = [Member(item) for item in data.get("items")]
+
+    return members
