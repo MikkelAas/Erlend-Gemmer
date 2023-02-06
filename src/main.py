e3c2cf8330839eff8bf1136b129f69bb55351454
Mikkel Aas <mikkel.aas@schibsted.com>
2023-02-06 19:19:51 +0100

    remove 'get_member_tag' function call

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 # Imports
 from services.clan import get_clan_info, get_war_status
 from services.player import get_player_info
-from database.database import get_member_tag
 import discord
 import os
 from dotenv import load_dotenv
@@ -41,7 +40,5 @@ async def on_message(message):
     elif message.content.startswith("$player"):
         name = message.content.split("$player ", 1)[1]
         await message.channel.send(embed=get_player_info(name))
-
-get_member_tag("Haspetre")
 
 client.run(bot_token)
