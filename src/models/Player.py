7d61e654281cce8b601268a71d981cb9f4b50d36
Mikkel Aas <mikkel.aas@schibsted.com>
2023-02-06 19:18:25 +0100

    remove 'town hall weapon level'

--- src/models/Player.py
+++ src/models/Player.py
@@ -4,7 +4,6 @@ class Player:
         self.attack_wins: int = data["attackWins"]
         self.defense_wins: int = data["defenseWins"]
         self.town_hall_level: int = data["attackWins"]
-        self.town_hall_weapon_level: int = data["townHallWeaponLevel"]
         self.versus_battle_wins: int = data["versusBattleWins"]
         self.donations: int = data["donations"]
         self.donations_recieved: int = data["donationsReceived"]
